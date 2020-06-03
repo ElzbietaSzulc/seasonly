f786f8e3844843bba14730eaa575f88dfd90a439
Elzbieta Szulc <elszulc@wp.pl>
2020-06-03 23:06:34 +0200

    Add top nav

--- src/assets/App.vue
+++ src/assets/App.vue
@@ -3,8 +3,6 @@
         <app-header></app-header>
         <hr>
         <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
         </div>
         <hr>
         <app-footer></app-footer>
